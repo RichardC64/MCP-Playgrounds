4afe7abb8008deefeec629419e06b5739d5e4c7b
Richard <rclark@c2i.fr>
2025-04-03 12:45:10 +0200

    Ajoutez des fichiers projet.

---
+++ McpPlaygroundServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"hello {message}";
+
+
+    [McpServerTool(Name = "SummarizeContentFromUrl"), Description("Summarizes content downloaded from a specific URI")]
+    public static async Task<string> SummarizeDownloadedContent(
+        IMcpServer thisServer,
+        HttpClient httpClient,
+        [Description("The url from which to download the content to summarize")]
+        string url,
+        CancellationToken cancellationToken)
+    {
+        var content = await httpClient.GetStringAsync(url);
+
+        ChatMessage[] messages =
+        [
+            new(ChatRole.User, "Briefly summarize the following downloaded content:"),
+            new(ChatRole.User, content),
+        ];
+
+        ChatOptions options = new()
+        {
+            MaxOutputTokens = 256,
+            Temperature = 0.3f,
+        };
+
+        return
+            $"Summary: {await thisServer.AsSamplingChatClient().GetResponseAsync(messages, options, cancellationToken)}";
+    }
+}
