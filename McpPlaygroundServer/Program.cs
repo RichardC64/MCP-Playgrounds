b502a9effe58523f5b90eb7505a28150439d50f2
rclark <rclark@c2i.fr>
2025-05-02 15:33:32 +0200

    Add logging level

--- McpPlaygroundServer/Program.cs
+++ McpPlaygroundServer/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ModelContextProtocol;
+using ModelContextProtocol.Protocol.Types;
 using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -11,10 +14,31 @@ builder.Services.AddSerilog(configure =>
     configure.WriteTo.File("logs/server_log.txt", rollingInterval: RollingInterval.Day);
 });
 
+var _minimumLoggingLevel = LoggingLevel.Debug;
+
 builder.Services
     .AddMcpServer()
+    .WithSetLoggingLevelHandler(async (ctx, token) =>
+    {
+        if (ctx.Params?.Level is null)
+        {
+            throw new McpException("Missing required argument 'level'", McpErrorCode.InvalidParams);
+        }
+
+        _minimumLoggingLevel = ctx.Params.Level;
+
+        await ctx.Server.SendNotificationAsync("notifications/message", new
+        {
+            Level = "debug",
+            Logger = "test-server",
+            Data = $"Logging level set to {_minimumLoggingLevel}",
+        }, cancellationToken: token);
+        return new EmptyResult();
+    })
     .WithStdioServerTransport()
     .WithToolsFromAssembly();
+
+builder.Services.AddSingleton<Func<LoggingLevel>>(_ => () => _minimumLoggingLevel);
 
 
 await builder.Build().RunAsync();
